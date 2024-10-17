627e6e9af7634c4f28bc98d8651369f9be213e57
Fernanda Passos <fernanda@midiacom.uff.br>
2024-10-17 16:46:12 +0100

    Examples

--- Lesson05/examples/test-http-post-fetch.mjs
+++ Lesson05/examples/test-http-post-fetch.mjs
@@ -2,14 +2,16 @@ let requestOptions = {
     method: 'POST',
     headers : {
         "Host": "postman-echo.com",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Content-Length": 27
+        //"Content-Type": "application/x-www-form-urlencoded",
+        //"Content-Length": 27
+        "Content-Type": "application/json",
     },
-    body : "field1=value1&field2=value2"
+    //body : "field1=value1&field2=value2"
+    body : JSON.stringify({field1: "value1", field2: "value2"})
   };
   
   fetch("http://postman-echo.com/post", requestOptions)
     .then(response => response.json())
     .then(objResponse => {console.log(objResponse.headers); return objResponse;})
-    .then(objResponse => console.log("-->", objResponse.form))
+    .then(objResponse => console.log("-->", objResponse.data))
     .catch(error => console.log('error', error));
